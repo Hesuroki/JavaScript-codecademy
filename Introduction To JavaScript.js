d6a19aac83b74bd05bb1a062ea0f78151cd11c0a
Hubert Rośniak <39415438+Hesuroki@users.noreply.github.com>
2019-01-15 12:32:51 +0100

    Add files via upload

--- Introduction To JavaScript.js
+++ Introduction To JavaScript.js
@@ -1,9 +1,54 @@
-/*Use the console.log code in the editor to log your age to the console.
+/* 1.
 
+1.Use the console.log code in the editor to log your age to the console.
 Run your code when you are ready to see the result.
-2.
 
-On the next line, log a number representing the number of weeks you've been programming. */
+2.On the next line, log a number representing the number of weeks you've been programming. */
+
+/* SOLUTION */
 
 console.log(24)
 console.log(24)
+
+
+/* 2
+
+1.Let's practice adding some code comments.
+To the right, we've provided you with the beginning of the book Catch-22 by Joseph Heller.
+On line 1, write a single line comment that says Opening line.
+
+
+2.Single line comments are great for adding context to your code. Multi-line comments are often best suited to prevent a block of code from running. However, both types of comments can be used for either purpose.
+
+Use a multi-line comment so that the console.log() statements on lines 4 - 9 are commented out. */
+
+/* SOLUTION */
+
+// Opening line.
+console.log('It was love at first sight.');
+/*
+console.log('The first time Yossarian saw the chaplain he fell madly in love with him.');
+console.log('Yossarian was in the hospital with a pain in his liver that fell just short of being jaundice.');
+console.log('The doctors were puzzled by the fact that it wasn\'t quite jaundice.');
+console.log('If it became jaundice they could treat it.');
+console.log('If it didn\'t become jaundice and went away they could discharge him.'); 
+console.log('But this just being short of jaundice all the time confused them.');
+*/
+
+
+/* 3.
+
+
+1.On line 1, log the string 'JavaScript' to the console.
+
+2.On line 2, log the number 2011 to the console.
+
+3.On line 3, print 'Woohoo! I love to code! #codecademy' to the console.
+
+4.On line 4, print the number 20.49 to the console. */
+
+console.log('JavaScript');
+console.log(2011);
+console.log('Woohoo! I love to code! #codecademy');
+console.log(20.49);
+
