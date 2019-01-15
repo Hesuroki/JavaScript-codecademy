6b92cb7f098ac1b70d162166543e0119333e10dc
Hubert Rośniak <39415438+Hesuroki@users.noreply.github.com>
2019-01-15 12:15:03 +0100

    Add files via upload

---
+++ Introduction To JavaScript.js
@@ -0,0 +1,9 @@
+/*Use the console.log code in the editor to log your age to the console.
+
+Run your code when you are ready to see the result.
+2.
+
+On the next line, log a number representing the number of weeks you've been programming. */
+
+console.log(24)
+console.log(24)
